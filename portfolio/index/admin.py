43aff284abf235c889ed3a9e593d2d6b32ecba40
Yashadatt Sawant <yashadatt.sawant@velotio.com>
2024-04-23 08:59:05 +0530

    DP-3 Make HTMLs Dynamic in nature

--- portfolio/index/admin.py
+++ portfolio/index/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import About
+from .models import About,Slider,Client,Service
 # Register your models here.
 
 admin.site.register(About)
+admin.site.register(Slider)
+admin.site.register(Service)
+admin.site.register(Client)
--- portfolio/index/views.py
+++ portfolio/index/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import About
+from .models import About,Slider,Client
 
 def home(request):
     about_data = About.objects.all()
     print(about_data[0].title)
-    context={
-        "about_data":about_data
+    slider_data = Slider.objects.all()
+    print(slider_data[0].title)
+    client_data = Client.objects.all()
+
+    context = {
+        "about_data": about_data,
+        "slider_data": slider_data,
+        "client_data": client_data
     }
     return render(request, "index.html", context)
 
